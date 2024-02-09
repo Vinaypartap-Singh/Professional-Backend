99273356063bd2d060972bc77c2a5c7be8ee005b
developervinay1 <developervsandhu@gmail.com>
2024-02-08 23:27:11 -0500

    Professional Setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import dotenv from "dotenv";
-import express from "express";
 import connectDB from "./db/index.js";
-const app = express();
+import { app } from "./app.js";
 
 // Optimized Code
 
@@ -9,7 +8,19 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error: ", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGO CONNECTION FAILED: ", error);
+  });
 
 // Better Approach to connect with db
 
