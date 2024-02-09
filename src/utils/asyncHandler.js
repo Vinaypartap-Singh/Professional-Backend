529a530aa38c3839346540999883b76b73fdbb09
developervinay1 <developervsandhu@gmail.com>
2024-02-08 23:50:16 -0500

    UTILS CODE DISCUSSION AND PROFESSIONAL APPROACH

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,24 @@
+// using promises
+
+const asyncHandler = async (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+      next(error)
+    );
+  };
+};
+
+export { asyncHandler };
+
+// Higher Order Function async handler
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
